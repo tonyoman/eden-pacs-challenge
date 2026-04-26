7671ff34635f77f495f523dd51fb008bf6645808
Tony Ordaz <tonyordaz@MacBook-Pro-de-Tony.local>
2026-04-26 17:49:05 -0600

    fix: add fallback URL for CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: process.env.MPR_URL,
+    baseURL: process.env.MPR_URL || 'https://pacs.evacenter.com/v2/mpr?studyId=738cb683-8426-42a1-b27e-921f455b70c4&tab=images&ac=dXNlcj1ldmEtY2VudGVyQHZpc2l0YW50LmNvbSZwYXNzd29yZD0zOTc5NzRlNS1hOWExLTQyOWYtYWRmMS02YzJkOTQ4ODhhYmImZXh0cmFfdmFsaWRhdGlvbj02ODkxNjNiYS0wMzAzLTRlYTEtYTRlMC1mNGE5Y2RiYTQ0YWQ%3D%3D%3D&md=1&serieId=0d018bf7-c18f-4882-b8fb-440fb72299d8&fromViewer=mobile_viewer',
     viewport: { width: 1920, height: 1080 },
     headless: true,
      trace: 'on-first-retry',
